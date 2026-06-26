cca45d7462c66332226fda802dfe03a43beb455f
Nikhil Sharma <nikhil.sharma28072005@gmail.com>
2026-06-26 15:36:06 +0530

    Update API URL to Render backend

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [departmentFilter, setDepartmentFilter] = useState("All");
   const [editingId, setEditingId] = useState(null);
 
-  const API_URL = "http://localhost:5100/employees";
+  const API_URL = "https://ems-backend-oglm.onrender.com/employees";
 
   // Fetch Employees
   const getEmployees = async () => {
